269bbfb1dc023a2fb87725cee79b63c120ede8b0
fauzannhidayat <fauzanhidayat773@gmail.com>
2023-11-08 21:51:01 +0700

    modified server.py

--- server.py
+++ server.py
@@ -9,9 +9,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/index.html')
-def main():
-    return render_template('index.html')
 
 @app.route('/indo')
 def init():
